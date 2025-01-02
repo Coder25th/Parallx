c76c3f5bd26a1f9f598a0d730d9236aafe11d2f8
Aryan <dev.404.me@gmail.com>
2025-01-02 15:48:55 +0530

    added cursor animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 import Parallax from "@/components/Parallax";
 import Lenis from "lenis";
+import { motion } from "framer-motion";
+import { AnimatePresence } from "motion/react";
+import Image from "next/image";
 import Link from "next/link";
+import metal from "@/public/berserk.jpg";
 import { useEffect } from "react";
+import useMousePosition from "@/utils/useMousePosition";
 
 const Home = () => {
   useEffect(() => {
@@ -14,15 +19,38 @@ const Home = () => {
     requestAnimationFrame(raf);
   }, []);
 
+  const { x, y } = useMousePosition();
+  const size = 200;
+
   return (
-    <main>
-      <Link
-        href="/parallax"
-        className="p-4 rounded-lg w-full bg-white text-black flex items-center justify-center"
+    <main className="h-[100vh] cursor-none">
+      <motion.div
+        className="text-gray-500 mask absolute w-[100%] h-[100%] items-center justify-center flex text-3xl leading-10"
+        animate={{
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 1.5}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut" }}
+        style={{
+          WebkitMaskImage: `url('/mask.svg')`, // Your SVG path
+          WebkitMaskRepeat: "no-repeat",
+          maskImage: `url('/mask.svg')`, // For cross-browser compatibility
+          maskRepeat: "no-repeat",
+          maskSize: `${size}px`,
+          backgroundColor: "red", // Mask fill color
+          color: "black",
+        }}
       >
-        Parallax
-      </Link>
-      <Parallax />
+        <p className="w-[1000px]">
+          Hello my name is aryan and this is a practice of my learing animation
+          with framer motion.
+        </p>
+      </motion.div>
+      <div className="w-[100%] h-[100%] items-center justify-center flex text-3xl leading-10">
+        <p className="w-[1000px]">
+          And i am enjoying it very much trying new things.This is amazing and i
+          hope you would also like it as much i do it.
+        </p>
+      </div>
     </main>
   );
 };
